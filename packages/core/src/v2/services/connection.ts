93b0ba687f8214c61b8b037f757c93347ca17353
hahaqwq <hahaqwq233@gmail.com>
2025-04-21 19:09:56 +0800

    fix: Fix the problem of the recursive call in the logout process

--- packages/core/src/v2/services/connection.ts
+++ packages/core/src/v2/services/connection.ts
@@ -167,7 +167,6 @@ export function createConnectionService(ctx: CoreContext) {
       }
 
       client.session.delete()
-      emitter.emit('auth:logout')
       logger.debug('Logged out from Telegram')
       return withResult(null, null)
     }
